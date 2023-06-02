213dc82ac4da77970c9fdf7e5025297140c77e4f
HARSH <mishraharshmishra2020@gmail.com>
2023-06-03 00:19:05 +0530

    A dance website .

--- DANCE WEBSITE/app.js
+++ DANCE WEBSITE/app.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const bodyparser = require("body-parser");
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/contactDance');
+
 const port = 80;
+
+//define mongoose schema
+    const contactSchema = new mongoose.Schema({
+        name: String,
+        phone: String,
+        email: String,
+        address: String,
+        desc: String,
+
+    });
+
+    const contact = mongoose.model('contact', contactSchema);
 
 
 // Express specific stuff
@@ -25,7 +45,19 @@ app.get('/contact',(req,res)=>{
     res.status(200).render('contact.pug',params);
 })
 
+    app.post('/contact', (req, res) => {
+
+        var mydata = new contact(req.body);
+        mydata.save().then(()=>{
+            res.send("This item has been saved to the database")
+        }).catch(()=>{
+            res.status(400).send("Item was not saved to the database")
+        })
+        // res.status(200).render('contact.pug');
+    })
+
 //Start the server
 app.listen(port,()=>{
     console.log(`The application started successfully on port ${port}`);
-})
+});
+}
